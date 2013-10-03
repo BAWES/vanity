9d91815d8eefe3bbabab139c20e0602cc5917c47
khalidmnet <khalid@khalidm.net>
2013-10-03 06:52:48 +0300

    Prepared password hashing and validation

--- protected/models/Agent.php
+++ protected/models/Agent.php
@@ -14,98 +14,122 @@
  * @property Region $region
  * @property Vanity[] $vanities
  */
-class Agent extends CActiveRecord
-{
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return 'agent';
-	}
+class Agent extends CActiveRecord {
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('region_id, agent_name, agent_email, agent_password', 'required'),
-			array('region_id', 'numerical', 'integerOnly'=>true),
-			array('agent_name, agent_password', 'length', 'max'=>160),
-			array('agent_email', 'length', 'max'=>180),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('agent_id, region_id, agent_name, agent_email, agent_password', 'safe', 'on'=>'search'),
-		);
-	}
+    private $salt = "28b206548469ce62182048fd9cf91760";
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-			'region' => array(self::BELONGS_TO, 'Region', 'region_id'),
-			'vanities' => array(self::HAS_MANY, 'Vanity', 'agent_id'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return 'agent';
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'agent_id' => 'Agent',
-			'region_id' => 'Region',
-			'agent_name' => 'Agent Name',
-			'agent_email' => 'Agent Email',
-			'agent_password' => 'Agent Password',
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('region_id, agent_name, agent_email, agent_password', 'required'),
+            array('agent_email', 'email'),
+            array('agent_password', 'rehashPassword', 'on' => 'changePw'),
+            array('region_id', 'numerical', 'integerOnly' => true),
+            array('agent_name, agent_password', 'length', 'max' => 160),
+            array('agent_email', 'length', 'max' => 180),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('agent_id, region_id, agent_name, agent_email, agent_password', 'safe', 'on' => 'search'),
+        );
+    }
+    
+    public function rehashPassword($attribute, $params) {
+        $this->agent_password = $this->hashPassword($this->agent_password, $this->salt);
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
+    protected function beforeSave() {
+        if (parent::beforeSave()) {
+            if ($this->isNewRecord)
+                $this->agent_password = $this->hashPassword($this->agent_password, $this->salt);
 
-		$criteria=new CDbCriteria;
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    //checks password param if equals to current users password
+    public function validatePassword($password) {
+        return $this->hashPassword($password, $this->salt) === $this->agent_password;
+    }
 
-		$criteria->compare('agent_id',$this->agent_id);
-		$criteria->compare('region_id',$this->region_id);
-		$criteria->compare('agent_name',$this->agent_name,true);
-		$criteria->compare('agent_email',$this->agent_email,true);
-		$criteria->compare('agent_password',$this->agent_password,true);
+    //hashes password input using given salt
+    public function hashPassword($password, $salt) {
+        return md5($salt . $password);
+    }
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array(
+            'region' => array(self::BELONGS_TO, 'Region', 'region_id'),
+            'vanities' => array(self::HAS_MANY, 'Vanity', 'agent_id'),
+        );
+    }
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return Agent the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'agent_id' => 'Agent',
+            'region_id' => 'Region',
+            'agent_name' => 'Agent Name',
+            'agent_email' => 'Agent Email',
+            'agent_password' => 'Agent Password',
+        );
+    }
+
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search() {
+        // @todo Please modify the following code to remove attributes that should not be searched.
+
+        $criteria = new CDbCriteria;
+
+        $criteria->compare('agent_id', $this->agent_id);
+        $criteria->compare('region_id', $this->region_id);
+        $criteria->compare('agent_name', $this->agent_name, true);
+        $criteria->compare('agent_email', $this->agent_email, true);
+        $criteria->compare('agent_password', $this->agent_password, true);
+
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return Agent the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
+
 }
